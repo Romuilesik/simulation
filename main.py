44a8aafde255c53790df9c2dbfbd5e4e11b13273
Romuilesik <roma.ilesik@gmail.com>
2023-02-12 12:49:07 +0200

    orel g reshka

---
+++ main.py
@@ -0,0 +1,14 @@
+import random
+
+live_cube = random.randint(1, 2)
+
+
+class orel:
+    if live_cube == 1:
+        print("orel")
+
+class reshka:
+    if live_cube == 2:
+        print("reshka")
+
+
